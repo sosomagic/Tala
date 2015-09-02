f4cc25748f8df3258406962e4f33f1940ebaa951
Chuck Wu <chwu@microstrategy.com>
2015-09-02 15:01:57 -0400

    create cleanup.py script

--- cleanup.py
+++ cleanup.py
@@ -1,1 +1,9 @@
-#This script is to delete all files and directories created old than a given date.
+#This script is to delete all files and directories created old than a given days.
+#This is a solution for usher build server, I will deploy this script to the server and run a periodical cleanup
+from sys import argv
+from subprocess import call
+
+script, file_path, days_older_than = argv
+print "\\"
+call(["find", file_path, "-mtime", "+"+days_older_than, "-exec", "rm", "-rf", "{}", "\\", ";"])
+
