11797ba8419b39f69f356104bfef88768743bfc4
Chuck Wu <chwu@microstrategy.com>
2015-09-02 15:53:42 -0400

    add the method to use xargs and pipe

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,13 @@
 #This script is to delete all files and directories created old than a given days.
 #This is a solution for usher build server, I will deploy this script to the server and run a periodical cleanup
 from sys import argv
-from subprocess import call
+import subprocess
 
 script, file_path, days_older_than = argv
+#This is using xargs command and PIPE
+files = subprocess.Popen(["find", file_path, "-mtime", "+"+days_older_than, "-print0"], stdout=subprocess.PIPE)
+subprocess.Popen (["xargs", "-0", "rm", "-rf"], stdin=files.stdout)
 
-call(["find", file_path, "-mtime", "+"+days_older_than, "-exec", "rm", "-rf", "{}", "\\", ";"])
+#This is using -exec command
+#subprocess.call(["find", file_path, "-mtime", "+"+days_older_than, "-exec", "rm", "-rf", "{}", "\\", ";"])
 
