9c2333ee0e21e54c4829506f90fe8e294a51be00
Chuck Wu <chwu@microstrategy.com>
2015-09-04 17:12:15 -0400

    add cleanup.py and test.py for Usher UBA

---
+++ uba_cleanup.py
@@ -0,0 +1,22 @@
+from sys import argv
+import subprocess
+import os
+import datetime
+import string
+import re
+
+#ask file_path, where you want to do the cleanup as the argv
+script, file_path = argv
+
+cleanup_date = (datetime.date.today() - datetime.timedelta(days=120)).strftime('%Y%m%d')
+print "I will delete the build files older than 2 months, but I will keep the latest build for each milestone."
+
+
+for dirname, dirnames, filenames in os.walk('.'):
+	for dir in dirnames:
+		if (re.match('\A\d\d\d\d\d\d\d\d_\d{0,4}', dir)):	
+			#print os.path.join(dirname,dir)
+			if cleanup_date > dir[0:8]:
+				dir_del = os.path.join(dirname, dir)
+				print dir_del + " is deleted."
+				subprocess.call(['rm', '-rf', dir_del])
