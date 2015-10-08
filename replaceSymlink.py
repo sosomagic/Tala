330477e796b369ec35087d7ee306547d698fe967
Chuck Wu <chwu@microstrategy.com>
2015-10-08 13:40:31 -0400

    update replaceSymlink.py

--- replaceSymlink.py
+++ replaceSymlink.py
@@ -8,6 +8,7 @@ script, rootPath, symFile = argv
 rootPath = os.path.normpath(rootPath)
 with open(symFile, mode='r') as file:
 	reader = csv.reader(file)
+	logger = open('error.txt', 'w')
 	for row in reader:
 		if row[0] == 'Link':	# This is the title row
 			continue
@@ -17,10 +18,17 @@ with open(symFile, mode='r') as file:
 		fromPath = os.path.join(rootPath, symFrom)
 		repo = symTo.split(os.path.sep)[1]
 		repoPath = os.path.join(rootPath, repo)
+		if not os.path.exists(toPath):
+			logger.write('Link file not exists: ' + toPath + '\n')
+			continue
+		if not os.path.exists(fromPath):
+			logger.write('Original file not exists: ' + fromPath + '\n')
+			continue
 		if repo == '3rdParty':
 			# cd to repository
 			os.chdir(repoPath)
 			# rm the existing symlink file or directory
 			subprocess.call(['git', 'rm', '-rf', toPath])
 			subprocess.call(['cp', '-r', fromPath, toPath])
+	logger.close()
 		
