3bb0e373fa9742b4511702f48dc20f6c52c10a17
Chuck Wu <chwu@microstrategy.com>
2015-10-08 14:08:40 -0400

    creat t.py

--- t.py
+++ t.py
@@ -1,3 +1,20 @@
 import os
+import csv
 
 print os.path.exists('/BIWeb/BIWebApp/code/html/MSTRWeb/WEB-INF')
+
+rootPath = '~/tmp/'
+
+with open('result.csv', mode='r') as file:
+	reader = csv.reader(file)
+	logger = open('error.txt', 'w')
+	for row in reader:
+		if row[0] == 'Link':	# This is the title row
+			continue
+		symTo = os.path.normpath(row[0])
+		toPath = os.path.join(rootPath, symTo)
+		symFrom = os.path.normpath(row[1])
+		fromPath = os.path.join(rootPath, symFrom)
+		if symFrom.startswith('/BIWeb/BIWebApp/code/html/MSTRWeb/WEB-INF'):
+			print fromPath
+			print os.path.exists(fromPath)
