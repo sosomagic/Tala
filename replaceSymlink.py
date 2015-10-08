939c84224d43ff5e5f9f3cd5a6cd713f24a87297
Chuck Wu <chwu@microstrategy.com>
2015-10-08 13:50:50 -0400

    update replaceSymlink.py

--- replaceSymlink.py
+++ replaceSymlink.py
@@ -18,18 +18,19 @@ with open(symFile, mode='r') as file:
 		fromPath = os.path.join(rootPath, symFrom)
 		repo = symTo.split(os.path.sep)[1]
 		repoPath = os.path.join(rootPath, repo)
-		if not os.path.exists(toPath):
-			logger.write('Link file not exists: ' + toPath + '\n')
-			continue
-		if not os.path.exists(fromPath):
-			logger.write('Original file not exists: ' + fromPath + '\n')
-			continue
 		if repo == '3rdParty' or repo == 'BIWeb':
-			# cd to repository
-			os.chdir(repoPath)
-			# rm the existing symlink file or directory
-			subprocess.call(['git', 'rm', '-rf', toPath])
-			subprocess.call(['cp', '-r', fromPath, toPath])
+			if not os.path.exists(toPath):
+				logger.write('Link file not exists: ' + toPath + '\n')
+				continue
+			elif not os.path.exists(fromPath):
+				logger.write('Original file not exists: ' + fromPath + '\n')
+				continue
+			else:
+				# cd to repository
+				os.chdir(repoPath)
+				# rm the existing symlink file or directory
+				subprocess.call(['git', 'rm', '-rf', toPath])
+				subprocess.call(['cp', '-r', fromPath, toPath])
 	logger.close()
 	os.chdir(rootPath)
 	subprocess.call(['repo', 'forall', '-c', 'git', 'add', '.'])
